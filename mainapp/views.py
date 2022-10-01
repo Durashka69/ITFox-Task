c774d6704d9b69dd6fe8747dd0f8eeceb29d9459
Durashka69 <gulyashshsh@gmail.com>
2022-10-01 22:21:43 +0600

    Changed permissions settings

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.parsers import MultiPartParser, FormParser
 
 from mainapp.models import User, Like, Comment, News
@@ -16,13 +16,13 @@ from mainapp.permissions import IsOwnerOrAdminOrReadOnly
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAdminUser,)
 
 
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
-    permission_classes = (IsOwnerOrAdminOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsOwnerOrAdminOrReadOnly,)
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
@@ -31,7 +31,7 @@ class LikeViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (IsOwnerOrAdminOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsOwnerOrAdminOrReadOnly)
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
@@ -41,7 +41,7 @@ class NewsViewSet(viewsets.ModelViewSet):
     parser_classes = [MultiPartParser, FormParser]
     queryset = News.objects.all()
     serializer_class = NewsSerializer
-    permission_classes = (IsOwnerOrAdminOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsOwnerOrAdminOrReadOnly)
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
