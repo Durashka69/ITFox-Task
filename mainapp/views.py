7653badccb672f087ec2f8a472853a51bec1940f
Durashka69 <gulyashshsh@gmail.com>
2022-10-01 23:28:15 +0600

    Added likes logic

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -23,6 +24,17 @@ class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
     permission_classes = (IsOwnerOrAdminOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        if Like.objects.filter(
+            user=self.request.user,
+            news=request.data.get('news')
+        ):
+            return Response(
+                {'message': f'Like from {request.user.username} already exists!'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
